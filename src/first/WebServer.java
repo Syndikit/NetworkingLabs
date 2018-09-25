88127c88bc39ebf0091e3d5ee2ffc70756c3f27b
Syndikit <maxaustin92@gmail.com>
2018-09-25 10:19:07 -0400

    Initial commit

---
+++ src/first/WebServer.java
@@ -0,0 +1,20 @@
+package first;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public final class WebServer {
+
+    public static void main(String[] args) throws Exception{
+
+    }
+
+    final class HttpRequest implements Runnable{
+
+        @Override
+        public void run() {
+            
+        }
+    }
+}
